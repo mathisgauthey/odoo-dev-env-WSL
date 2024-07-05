29473f287c2dd1767ae917612c0a8876f4c620d9
Mathis Gauthey <mgauthey@okteo.fr>
2024-07-05 16:00:08 +0200

    build: :building_construction: add default `odoo.conf` and stop using `.env` files for nothing
    I won't use `.env` file anymore as they don't allow for variables in launch args both for Intellij and VSCODE.
    
    Instead, create a run config by project with its own :
    - ADDONS_PATH (for --addons-path)
    - ADDONS_LIST (for -i and -u)
    - DB_NAME (for a different db per project)
    - PROJECT.conf file (for addons path for PyTests)

--- drop_db.sh
+++ drop_db.sh
@@ -1,2 +1,6 @@
 sudo service postgresql restart
-sudo -u postgres psql postgres -c 'DROP DATABASE "odoo-db";'
+echo "List of databases owned by odoo: "
+sudo -u postgres psql -t -c "SELECT datname FROM pg_database WHERE datdba = (SELECT oid FROM pg_roles WHERE rolname='odoo');"
+echo "Enter database name to drop: "
+read DB_NAME
+sudo -u postgres psql -c "DROP DATABASE \"$DB_NAME\";"
